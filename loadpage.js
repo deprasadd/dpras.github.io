482ed321eec22ba9d3358e67ce0ef233641865b7
deprasadd <dhruveprasad@gmail.com>
2023-07-12 19:14:21 -0400

    URL Fragments and page load
    Deprecated full URLs and opted for fragments instead to keep cursor from resetting position; also made the website continuous and house all content for each page in index.html.

--- loadpage.js
+++ loadpage.js
@@ -1,18 +1,38 @@
-$(document).ready(function() {
-    $(".nav-link").click(function(e) {
-      e.preventDefault();
-      const page = $(this).data("page");
-      switchPage(page);
-    });
-  
-    const initialPage = window.location.hash.substr(1);
-    loadPage(initialPage);
-  });
-  
-  function switchPage(page) {
-    window.location.hash = page;
+// Function to handle the navigation based on URL fragments
+function handleNavigation() {
+  // Get the current URL fragment
+  var fragment = window.location.hash;
+
+  // Set the default fragment to #home if no fragment is provided
+  if (fragment === '') {
+      window.location.hash = '#home'; // Redirect to index.html#home
+      return;
   }
-  
-  function loadPage(page) {
-    $("#content").load(page + ".html");
+
+  // Load the appropriate content based on the fragment
+  if (fragment === '#home') {
+      // Load home.html content
+      loadContent('home-template');
+  } else if (fragment === '#about') {
+      // Load about.html content
+      loadContent('about-template');
+  } else if (fragment === '#cv') {
+      // Load cv.html content
+      loadContent('cv-template');
+  } else {
+      // Handle invalid fragment, show an error message or redirect to a default page
+      document.getElementById('content').innerHTML = '<h2>Invalid Page</h2>';
   }
+}
+
+// Function to load the content from the specified template
+function loadContent(templateId) {
+  var template = document.getElementById(templateId);
+  if (template) {
+      document.getElementById('content').innerHTML = template.innerHTML;
+  }
+}
+
+// Call the handleNavigation function when the page loads or when the URL fragment changes
+window.addEventListener('DOMContentLoaded', handleNavigation);
+window.addEventListener('hashchange', handleNavigation);
